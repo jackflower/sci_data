4b35343f9b1dbce03e682570fd26b591b689f6c4
jackflower <jackflower@poczta.onet.pl>
2019-10-16 12:58:53 +0200

    Update
    Wrzucam przykładowe pliki

---
+++ sci_functions/Function_sci.cpp
@@ -0,0 +1,9 @@
+#include "Function_sci.h"
+
+//Funkcja zamienia parametry a z b
+void swap(int & a, int & b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
---
+++ sci_functions/Function_sci.h
@@ -0,0 +1,17 @@
+#ifndef H_FUNCTIONS_SCI
+#define H_FUNCTIONS_SCI
+
+//Jack Flower - SCI
+//Created Obtober, 2019
+
+///
+///Funkcja zamienia parametry a z b
+///
+///@param a - pierwszy parametr (referencja)
+///
+///@param b - drugi parametr (referencja)
+///
+void swap(int & a, int & b);
+
+#endif//H_FUNCTIONS_SCI
+
